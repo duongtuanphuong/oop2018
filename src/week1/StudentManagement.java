6a94aee431c6aba97ee8f54085f1865879fba5b8
duongtuanphuong <43140650+duongtuanphuong@users.noreply.github.com>
2018-09-19 18:37:52 +0700

    ADD

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -2,7 +2,11 @@ package week1;
 
 public class StudentManagement {
     Student[] St1 = new Student[100];
-
+    int a=0;
+    void add(Student sv)
+        {
+            St1[a++]=sv;
+        }
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
 
     public boolean sameGroup(Student s1, Student s2) {
@@ -12,17 +16,43 @@ public class StudentManagement {
     }
 
     void studentsByGroup() {
+        for (int i=0;i<St1.length-1;i++){
+            for (int j=0;j<St1.length;j++){
+                if(St1[i].getGroup().compareTo(St1[j].getGroup())<0){
+                    Student A = new Student();
+                    A=St1[i];
+                    St1[i]=St1[j];
+                    St1[j]=A;
+                }
+            }
+        }
+        String group=" ";
+        int i=0,n=St1.length;
+        while (i<n){
+            if (St1[i].getGroup().equals(group)== false){
+                group =St1[i].getGroup();
+                System.out.println("cac sinh vien thuoc lop " +group +": ");
+                System.out.println(St1[i]);
+            }
+            else {
+                System.out.println(St1[i]);
+            }
+            i++;
+        }
 
         // TODO:
     }
 
     void removeStudent(String id) {
-        int n = St1.length;
-        for (int i=0;i<n;i++){
-            if (St1[i].getId()==id);{
-                n--;
-                for (int j=i;j<n;j++) St1[j]=St1[j+1];
-            }
+          for (int i=0;i<a;i++){
+              if (St1[i].getId().equals(id)){
+                  for (int j=i;j<a-1;j++){
+                      St1[j]=St1[j+1];
+
+                  }
+                a--;
+              }
+
         }
 
         // TODO:
@@ -50,6 +80,9 @@ public class StudentManagement {
         System.out.println("Student s01 va s02  " +ss.sameGroup(s01,s02));
         System.out.println("Student s01 va s03  " +ss.sameGroup(s01,s03));
         System.out.println("Student s02 va s03  " +ss.sameGroup(s02,s03));
+
+        
+
 
 
 
