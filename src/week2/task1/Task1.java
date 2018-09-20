ecff9614a5e6de2e6b949276145bba855106f5d8
duongtuanphuong <43140650+duongtuanphuong@users.noreply.github.com>
2018-09-20 19:41:12 +0700

    ADD

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,8 +1,13 @@
 package week2.task1;
 
+import java.util.Scanner;
+
 public class Task1 {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static int gcd(int a, int b) {
+
+
         // TODO: Tính ước chung lớn nhất của 2 số a, b
         return 0;
     }
@@ -10,5 +15,13 @@ public class Task1 {
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
         return 0;
+    }
+    public static void main(String[] args){
+        System.out.println("nhap so a :");
+        int a =scanner.nextInt();
+        System.out.println("nhap so b :");
+        int b =scanner.nextInt();
+        System.out.println("uoc chung lon nhat cua a va b la :"+ gcd(a,b));
+
     }
 }
