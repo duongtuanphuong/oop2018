c433410f6a19f7bec79734ad3830a658f2420e25
duongtuanphuong <43140650+duongtuanphuong@users.noreply.github.com>
2018-09-24 13:14:47 +0700

    ADD

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -6,22 +6,34 @@ public class Task1 {
     private static Scanner scanner = new Scanner(System.in);
 
     public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
 
 
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
-    }
+        }
 
     public static int fibonacci(int n) {
+            if (n==0 ) return 0;
+            else if (n==1) return 1;
+            else  {
+                return fibonacci(n-1)+fibonacci(n-2);
+            }
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
-    }
+            }
     public static void main(String[] args){
         System.out.println("nhap so a :");
         int a =scanner.nextInt();
         System.out.println("nhap so b :");
         int b =scanner.nextInt();
         System.out.println("uoc chung lon nhat cua a va b la :"+ gcd(a,b));
+        int n=scanner.nextInt();
+        System.out.println("day fibonaci la :" );
+        for (int i=0; i<n; i++){
+            System.out.println( +fibonacci(i)+" ");
+        }
+
+
 
     }
 }
