5351c01c72b1b8eafdd8705949941ad7aacd2f78
duongtuanphuong <43140650+duongtuanphuong@users.noreply.github.com>
2018-09-26 18:18:12 +0700

    Nộp bài tập

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -2,6 +2,18 @@ package week2.task2;
 
 public class Fraction {
     private int numerator,denominator;
+    public void SetNumerator(int a){
+        numerator=a;
+    }
+    public int setNumerator(){
+        return numerator;
+    }
+    public void getDenominator(int b){
+        denominator=b;
+    }
+    public int getDenominator(){
+        return denominator;
+    }
 
     // TODO: khai báo các thuộc tính
 
@@ -15,7 +27,7 @@ public class Fraction {
         Fraction sum = new Fraction(1,1);
         sum.numerator = this.numerator*other.denominator+other.numerator*this.denominator;
         sum.denominator=this.denominator*other.denominator;
-
+        System.out.println(sum.numerator + "/" + sum.denominator);
         return sum;
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
 
@@ -25,6 +37,7 @@ public class Fraction {
         Fraction subtract = new Fraction (1,1);
         subtract.numerator=this.numerator*other.denominator-other.numerator*this.denominator;
         subtract.denominator=this.denominator*other.denominator;
+        System.out.println(subtract.numerator + "/" + subtract.denominator);
         return subtract;
 
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
@@ -34,7 +47,7 @@ public class Fraction {
         Fraction multiply = new Fraction(1,1);
         multiply.numerator=this.numerator*other.numerator;
         multiply.denominator=this.denominator*other.denominator;
-
+        System.out.println(multiply.numerator + "/" + multiply.denominator);
         return multiply;
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
 
@@ -44,47 +57,24 @@ public class Fraction {
         Fraction divide=new Fraction(1,1);
         divide.numerator=this.numerator*other.denominator;
         divide.denominator=this.denominator*other.numerator;
+        System.out.println(divide.numerator + "/" + divide.denominator);
 
         return divide;
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
 
     }
-    public static void rutgon(Fraction n){
-            int a=n.numerator;
-            int b=n.denominator;
-            int c;
-            while(b!=0){
-                c=a%b;
-                a=b;
-                b=c;
-            }
-            n.denominator /= a;
-            n.numerator /= a;
-    }
-    public boolean equals(Fraction other){
-        Fraction n =new Fraction(this.numerator,this.denominator);
-        rutgon(n);
-        rutgon(other);
-        if (this.numerator == other.numerator && this.denominator==other.denominator)
-            return true;
-        else return false;
+
+    public boolean equals(Object obj){
+        Fraction a = (Fraction) obj;
+        return (numerator*a.denominator == a.numerator*denominator);
 
     }
-    public void xuat(){
-        System.out.println(numerator + "/" +denominator);
-    }
+
     public static void main(String[] args){
         Fraction a = new Fraction(12,34);
         Fraction b = new Fraction(2,9);
-        System.out.println("Ket qua phep cong la :");
-        a.add(b).xuat();
-        System.out.println("Ket qua phep tru la :");
-        a.subtract(b).xuat();
-        System.out.println("Ket qua phep nhan la :");
-        a.multiply(b).xuat();
-        System.out.println("Ket qua phep chia la :");
-        a.divide(b).xuat();
-        System.out.println("Hai phan so bang nhau :"+a.equals(b));
+        Fraction c = new Fraction(1,1);
+
 
 
        //*result = a.subtract(b);
