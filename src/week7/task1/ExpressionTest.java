b97148a9c26f65ea696f339b7761a09ba33c0c73
duongtuanphuong <binonljne04@gmail.com>
2018-10-31 18:46:03 +0700

    Nộp bài tập

--- src/week7/task1/ExpressionTest.java
+++ src/week7/task1/ExpressionTest.java
@@ -7,6 +7,16 @@ public class ExpressionTest {
        Multiplication c = new Multiplication(new Numeral(2), new Numeral(3));
        Addition d = new Addition(b,c);
        Square f =new Square(d);
+       //(10^2-1+2*3)^2==11025
        System.out.println(f.evaluate());
+        Division div =new Division(new Numeral(6), new Numeral(0));
+        try{
+            double x;
+            x= div.left.evaluate()/div.right.evaluate();
+            System.out.println(x);
+        }catch (ArithmeticException e ){
+            System.out.println("Không thể chia cho số 0");
+        }
+
     }
 }
--- src/week7/task2/Task2.java
+++ src/week7/task2/Task2.java
@@ -1,4 +1,31 @@
 package week7.task2;
 
 public class Task2 {
+    class Meo{
+        public void Voice(){
+
+        }
+    }
+
+
+    public double ArithmeticException(double x,double y) {
+        if(y==0) throw  new ArithmeticException();
+        return x/y;
+    }
+    public boolean NullPointerException(String a, String b) throws NullPointerException{
+        if(a==null) throw new NullPointerException();
+        return a.equals(b);
+    }
+
+
+
+    public static void main(String[] args) {
+        Task2 task2 =new Task2();
+        try{
+            System.out.println(task2.ArithmeticException(2,0));
+        }catch (ArithmeticException e){
+            System.out.println("Lỗi ArithmeticException ");
+        }
+
+    }
 }
